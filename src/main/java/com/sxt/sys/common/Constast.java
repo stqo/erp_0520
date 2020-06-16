8e4ac4052f74bdd9908b9837b49f3f1794a66b89
stqo <1440729422@qq.com>
2020-06-16 23:06:16 +0800

    更新

--- src/main/java/com/sxt/sys/common/Constast.java
+++ src/main/java/com/sxt/sys/common/Constast.java
@@ -1,5 +1,7 @@
 package com.sxt.sys.common;
 
+import javax.print.attribute.standard.NumberUp;
+
 public interface Constast {
 	
 	
@@ -9,5 +11,35 @@ public interface Constast {
 	 */
 	public static final Integer OK=200;
 	public static final Integer ERROR=-1;
+
+
+	/**
+	 * 菜单权限类型
+	 */
+	public static final String TYPE_MNEU = "menu";
+	public static final String TYPE_PERMISSION = "permission";
+
+
+	/**
+	 * 可用状态
+	 */
+	public static final Object AVAILABLE_TYPE = 1;
+	public static final Object AVAILABLE_FALSE = 0;
+
+
+	/**
+	 * 用户类型
+	 * 根据是<超级管理员>还是<普通用户>决定菜单的多少
+	 */
+	public static final Object USER_TYPE_SUPER = 0;
+	public static final Object USER_TYPE_NORMAL = 1;
+
+
+	/**
+	 *
+	 */
+
+	public static final Integer OPEN_TRUE = 0;
+	public static final Integer OPEN_FALSE = 1;
 
 }
