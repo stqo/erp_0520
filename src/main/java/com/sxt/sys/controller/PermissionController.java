b06a6a185b9153bf39624e9d02db7ae2ef34725d
stqo <1440729422@qq.com>
2020-06-16 19:28:28 +0800

    更新

--- src/main/java/com/sxt/sys/controller/PermissionController.java
+++ src/main/java/com/sxt/sys/controller/PermissionController.java
@@ -1,9 +1,14 @@
 package com.sxt.sys.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @2020/6/16 - 18:44
- * @value stqo
+ * @value stqo   前端控制器
  */
+@RestController
+@RequestMapping("/sys/permission")
 public class PermissionController {
 
 }
